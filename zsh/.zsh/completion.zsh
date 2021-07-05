c39b7e0dfd044c0aa172659c1be6f5912aeaf0db
Qinabu <qinabu@gmail.com>
2021-07-05 13:20:08 +0300

    /Users/alex

--- zsh/.zsh/completion.zsh
+++ zsh/.zsh/completion.zsh
@@ -1,4 +1,6 @@
 autoload -U compinit
 compinit
 zstyle ':completion:*' completer _expand_alias _complete _ignored _approximate _correct
-zstyle ':completion:*:approximate:*' max-errors 5
+# zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+zstyle ':completion:*:approximate:*' max-errors 5 not-numeric

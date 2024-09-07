731c8a92aefa175e95501e62f510f30ba45403d4
qinabu <qinabu@gmail.com>
2024-09-08 00:12:32 +0300

    _

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -853,7 +853,7 @@ function F.gen()
 	-- ollama gen
 	-- llama2-uncensored
 	-- require('gen').model = 'llama2-uncensored'
-	require('gen').model = 'llama3.1'
+	require('gen').model = 'gemma2:2b' -- llama3.1
 	require('gen').display_mode = "split"
 	require('gen').show_prompt = true
 	-- require('gen').container = nil
